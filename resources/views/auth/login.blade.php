23caa525754a2e13d3f9e932956e8c661940b10e
Mark Ayllon Jaime <mark.frontdevops@gmail.com>
2025-05-12 15:28:22 +0200

    Push

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,8 +11,8 @@
 <body>
     <form method="POST" action="{{ route('login') }}">
         @csrf
-        <input type="email" name="email" placeholder="Email">
-        <input type="password" name="password" placeholder="Password">
+        <input type="email" name="email" placeholder="Email" required>
+        <input type="password" name="password" placeholder="Password" required>
         <button type="submit">Login</button>
     </form>
 </body>
